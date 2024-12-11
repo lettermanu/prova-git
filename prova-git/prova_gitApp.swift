0c0ba2e78d15ecd8a8145e542bbbc025cfd9bd62
Manuela Letteriello <m.letteriello@yahoo.it>
2024-12-11 14:53:11 +0100

    Initial Commit

---
+++ prova-git/prova_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  prova_gitApp.swift
+//  prova-git
+//
+//  Created by Manuela Letteriello on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct prova_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
